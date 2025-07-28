a4f4d7929160d4138567d4752bd04476b47cc0e6
M. Ahsan Wiryawan <ahsan.wiryawan@gmail.com>
2025-07-28 17:57:46 +0700

    Create friends.php

---
+++ 3. hard-vulnerable-lab/website-codebase/friends.php
@@ -0,0 +1,3 @@
+<!DOCTYPE html><html><head><title>Friends - SocialGram</title><link rel="stylesheet" href="css/style.css"><link href="https://fonts.googleapis.com/css?family=Grand+Hotel" rel="stylesheet"></head><body>
+<div class="navbar"><a href="feed.php" class="logo">SocialGram</a><div class="navbar-links"><a href="profile.php">My Profile</a><a href="logout.php">Logout</a></div></div>
+<main class="container"><div class="sidebar-box" style="text-align:center;"><h1>Friends</h1><p>This feature is coming soon!</p></div></main></body></html>
