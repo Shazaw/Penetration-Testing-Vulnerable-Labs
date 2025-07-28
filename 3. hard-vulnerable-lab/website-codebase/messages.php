0893f3cc61272e296ad3aeddebfb694f599d2f5d
M. Ahsan Wiryawan <ahsan.wiryawan@gmail.com>
2025-07-28 17:57:25 +0700

    Create messages.php

---
+++ 3. hard-vulnerable-lab/website-codebase/messages.php
@@ -0,0 +1,3 @@
+<!DOCTYPE html><html><head><title>Messages - SocialGram</title><link rel="stylesheet" href="css/style.css"><link href="https://fonts.googleapis.com/css?family=Grand+Hotel" rel="stylesheet"></head><body>
+<div class="navbar"><a href="feed.php" class="logo">SocialGram</a><div class="navbar-links"><a href="profile.php">My Profile</a><a href="logout.php">Logout</a></div></div>
+<main class="container"><div class="sidebar-box" style="text-align:center;"><h1>Messages</h1><p>This feature is coming soon!</p></div></main></body></html>
